91cfd4231f7a954856a1bf10e409b2ba5dfbf441
pankaj.sharma <pankaj.sharma@hiddenbrains.in>
2018-03-12 20:02:39 +0530

    Updated to 2.0

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.example.databinding.ItemBinding;
 import com.simpleadapter.SimpleAdapter;
@@ -12,12 +13,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    final ArrayList<User> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ArrayList<User> list = new ArrayList<>();
 
         list.add(new User("Pankaj"));
         list.add(new User("Sumit"));
@@ -30,5 +32,21 @@ public class MainActivity extends AppCompatActivity {
                 binding.text.setText(model.name);
             }
         }));
+        recyclerView.setAdapter(SimpleAdapter.with(R.layout.item, list, new SimpleAdapter.SmartBinder<User, ItemBinding>() {
+            @Override
+            public void onBind(int position, User model, ItemBinding binding) {
+                binding.text.setText(model.name);
+            }
+
+            @Override
+            public void onClick(View view, User model, int position) {
+                super.onClick(view, model, position);
+            }
+
+            @Override
+            public void onCheckedChange(View view, User model, int position) {
+                super.onCheckedChange(view, model, position);
+            }
+        }).setClickableViews(R.id.text).setCheckableView(R.id.checkbox));
     }
 }
--- app/src/main/java/com/example/MyActivity.kt
+++ app/src/main/java/com/example/MyActivity.kt
@@ -4,25 +4,41 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.example.databinding.ItemBinding
+import com.simpleadapter.SimpleAdapter
 import com.simpleadapter.SimpleAdapter.with
 import java.util.*
 
 class MyActivity : AppCompatActivity() {
 
-    val list = ArrayList<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = ArrayList<User>()
 
         list.add(User("Pankaj"))
         list.add(User("Sumit"))
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this@MyActivity)
-        recyclerView.adapter = with(R.layout.item, list, { position: Int, model: User, binding: ItemBinding ->
+        /*recyclerView.adapter = with(R.layout.item, list, { position: Int, model: User, binding: ItemBinding ->
             binding.text.setText(model.name)
-        })
+        })*/
+        recyclerView.adapter = with(R.layout.item, list, object : SimpleAdapter.SmartBinder<User, ItemBinding>() {
+            override fun onBind(position: Int, model: User, binding: ItemBinding) {
+                binding.text.setText(model.name)
+            }
+
+            override fun onClick(view: View?, model: User?, position: Int) {
+                super.onClick(view, model, position)
+            }
+
+            override fun onCheckedChange(view: View?, model: User?, position: Int) {
+                super.onCheckedChange(view, model, position)
+            }
+        }).setClickableViews(R.id.text).setCheckableView(R.id.checkbox)
     }
 }
